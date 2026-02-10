9fef52b0e9b26bda5271c080e1dac5e4bb14887e
akki-g <akshat.guduru@gmail.com>
2026-02-10 13:01:20 -0500

    completed useIndicatorInfo hook

--- frontend/src/hooks/useIndicatorInfo.ts
+++ frontend/src/hooks/useIndicatorInfo.ts
@@ -10,3 +10,58 @@ interface UseIndicatorInfoResult {
     refetch: () => Promise<void>;
 }
 
+export function UseIndicatorInfo(indicator_name: string): UseIndicatorInfoResult {
+    const [data, setData] = useState<IndicatorDetail | null> (null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<APIError | null> (null);
+    const abortControllerRef = useRef<AbortController | null> (null);
+
+    const fetchIndicatorDetails = useCallback( async () => {
+
+        if (!indicator_name || indicator_name.trim().length === 0){
+            setData(null);
+            setLoading(false);
+            setError(null);
+            return;
+        }
+
+        if (abortControllerRef.current){
+            abortControllerRef.current.abort();
+        }
+
+        setLoading(true);
+        setError(null);
+        abortControllerRef.current = new AbortController();
+
+        try { 
+            const indicatorInfo = await fetchIndicatorInfo(indicator_name, abortControllerRef.current.signal);
+            setData(indicatorInfo);
+            setLoading(false);
+        }
+        catch (error) {
+            if ((error as Error).name === 'AbortError'){
+                return;
+            }
+            setError({
+                message: (error as APIError).message || `Failed to fetch indicator ${encodeURIComponent(indicator_name)}'s info`,
+                status: (error as APIError).status,
+                details: (error as APIError).details
+            })
+
+            setData(null);
+            setLoading(false);
+        }
+    }, [indicator_name]);
+
+    useEffect(() => {
+        fetchIndicatorDetails();
+
+        return () => {
+            if (abortControllerRef.current){
+                abortControllerRef.current.abort();
+            }
+        }
+    }, [fetchIndicatorInfo]);
+
+    return {data, loading, error, refetch: fetchIndicatorDetails}
+}
